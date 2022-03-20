75511f724a59e1899f252377a6109c22d5a7455b
Sertay Sener <sertaysener@Sertays-MBP.hitronhub.home>
2022-03-19 18:50:23 -0700

    update project

--- project-6/truffle-config.js
+++ project-6/truffle-config.js
@@ -1,14 +1,24 @@
+const HDWalletProvider = require('truffle-hdwallet-provider')
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*" // Match any network id
-    }
+      network_id: "*",
+    },
+
+    rinkeby: {
+      provider: () => new HDWalletProvider("my phrase", 
+      `https://rinkeby.infura.io/v3/<endpoint>`),
+      
+      network_id: 4,
+    },
   },
+
   compilers: {
     solc: {
-      version: "^0.8.0"
+      version: "0.8.0"
     }
-  }
-};
+ }
+}
